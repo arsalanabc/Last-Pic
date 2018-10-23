d445cb15a6246e623edbf6f761280bbfffa310e9
arsalan.ahmed <arsalanabc@gmail.com>
2018-10-23 18:00:53 +0100

    Asking for permission to read MediaStorage, reading the last image taken and showing on the screen.

---
+++ app/src/main/java/com/example/arsalan/last_pic/RunTimePermission.java
@@ -0,0 +1,57 @@
+package com.example.arsalan.last_pic;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+public abstract class RunTimePermission extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int permission : grantResults) {
+            permissionCheck = permissionCheck + permission;
+        }
+        if ((grantResults.length > 0) && permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            onPermissionsGranted(requestCode);
+        } else {
+            Toast.makeText(this, "Permissions denied", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void requestAppPermissions(final String[] permissions, final int requestCode) {
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        boolean shouldShowRequestPermissionRationale = false;
+        for (String permission : permissions){
+
+            permissionCheck = permissionCheck + ContextCompat.checkSelfPermission(this, permission);
+            shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale) {
+                    Toast.makeText(RunTimePermission.this, "We need %s to make this app work", Toast.LENGTH_SHORT).show();
+                } else {
+                    ActivityCompat.requestPermissions(this, permissions, requestCode);
+                }
+            }
+        }
+
+    }
+
+    public void onPermissionsGranted (int requectCode) {
+        Toast.makeText(this, "Permissions Received.", Toast.LENGTH_LONG).show();
+    }
+}
