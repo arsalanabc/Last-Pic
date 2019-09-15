c384f5380cb243548d63bc8c640cee525e8e0a59
arsalan.ahmed <arsalanabc@gmail.com>
2019-09-15 16:41:50 -0400

    make AndroidId static so we can get userId without instantiating the object

--- app/src/main/java/com/example/arsalan/last_pic/Model/AndroidId.java
+++ app/src/main/java/com/example/arsalan/last_pic/Model/AndroidId.java
@@ -4,14 +4,10 @@ import android.app.Activity;
 import android.provider.Settings;
 
 public class AndroidId {
-    private String value;
 
-    public AndroidId(Activity activity) {
-        this.value = Settings.Secure.getString(activity.getContentResolver(),
+    public static String getAndroidId(Activity activity) {
+        return Settings.Secure.getString(activity.getContentResolver(),
         Settings.Secure.ANDROID_ID);
     }
 
-    public String getValue() {
-        return value;
-    }
 }
