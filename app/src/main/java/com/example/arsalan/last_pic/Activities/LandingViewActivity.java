3eaa8511e7c643498041cefe4139083238c4bdb9
arsalan.ahmed <arsalanabc@gmail.com>
2019-09-15 19:47:37 -0400

    add logo screen and 3 sliding screens to show what this app is about NOTE: tutorial https://javapapers.com/android/android-intro-slider-and-splash-screen/

---
+++ app/src/main/java/com/example/arsalan/last_pic/Activities/LandingViewActivity.java
@@ -0,0 +1,41 @@
+package com.example.arsalan.last_pic.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.arsalan.last_pic.R;
+
+//TODO change this activity to be the launcher
+public class LandingViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_screen);
+        hideBar();
+        Thread myThread = new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(1200);
+                    Intent intent = new Intent(getApplicationContext(), SlidingActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+    private void hideBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+}
