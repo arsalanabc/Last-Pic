bc46f473d94ee7b6a57af1718434fae14114c296
arsalan.ahmed <arsalanabc@gmail.com>
2019-09-15 16:52:57 -0400

    check whether the deviceURL has changed. If the user hasnt not taken a new image, no need to update the database

--- app/src/main/java/com/example/arsalan/last_pic/UploadActivity.java
+++ app/src/main/java/com/example/arsalan/last_pic/UploadActivity.java
@@ -7,13 +7,22 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 
+import com.example.arsalan.last_pic.Model.AndroidId;
+import com.example.arsalan.last_pic.Model.LastPic;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.File;
 
 public class UploadActivity extends Activity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,18 +40,47 @@ public class UploadActivity extends Activity {
             String imagePath = imageCursor.getString(columnIndex);
             File imageFile = new File(imagePath);
 
-            ImageUploader imageUploader = new ImageUploader(imagePath, this, mFirebaseAnalytics);
 
             if (imageFile.canRead() && imageFile.exists()) {
                 // we have found the latest picture in the public folder, do whatever you want
 
-                imageUploader.execute(Uri.fromFile(new File(imagePath)));
+                checkIfImageNeedsToBeUpdated(imagePath);
                 startActivity(new Intent(this, ImageViewer.class));
                 break;
             }
-
         }
-
     }
 
+    private void checkIfImageNeedsToBeUpdated(final String currentImagePath){
+        String userId = AndroidId.getAndroidId(this);
+        Query query = dbRef.child("last_pic").orderByChild("userId").equalTo(userId);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                             if(!data.getValue(LastPic.class)
+                                     .getDeviceURL().equals(currentImagePath)){
+                                 updateImage(currentImagePath);
+                             } else {
+                                 noUpdateNeeded();
+                             }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void noUpdateNeeded() {
+        this.finish();
+    }
+
+    private void updateImage(String imagePath) {
+        ImageUploader imageUploader = new ImageUploader(imagePath, this, mFirebaseAnalytics);
+        imageUploader.execute(Uri.fromFile(new File(imagePath)));
+    }
 }
