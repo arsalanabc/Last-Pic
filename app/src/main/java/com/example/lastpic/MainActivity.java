c3cfb4e20e0f55118e7b5d30c289fe6ef4c580da
arsalan <arsalanabc@gmail.com>
2019-09-17 16:59:41 -0400

    add google analytics to the MainActivity to track number of times the app is launched

--- app/src/main/java/com/example/lastpic/MainActivity.java
+++ app/src/main/java/com/example/lastpic/MainActivity.java
@@ -3,16 +3,31 @@ package com.example.lastpic;
 import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.util.Log;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 public class MainActivity extends RunTimePermission {
 
     private static final int REQUEST_PERMISSIONS = 20;
+    private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         hideBar();
+
+        // Obtain the shared Tracker instance.
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+        mTracker.setScreenName("MainActivity Launched");
+        mTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("APP_LAUNCHED")
+                .setLabel("ENTERED")
+                .build());
 
         MainActivity.super.requestAppPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
                 REQUEST_PERMISSIONS);
