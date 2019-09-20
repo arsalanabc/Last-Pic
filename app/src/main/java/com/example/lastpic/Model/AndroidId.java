5792876922c08c20d466a91437a054e38dd65374
arsalan <arsalanabc@gmail.com>
2019-09-20 05:57:29 -0400

    add a new field to Android Id static class so we can store the id and get it in non-activity classes

--- app/src/main/java/com/example/lastpic/Model/AndroidId.java
+++ app/src/main/java/com/example/lastpic/Model/AndroidId.java
@@ -4,10 +4,17 @@ import android.app.Activity;
 import android.provider.Settings;
 
 public class AndroidId {
+    static String id;
 
     public static String getAndroidId(Activity activity) {
-        return Settings.Secure.getString(activity.getContentResolver(),
+
+        id = Settings.Secure.getString(activity.getContentResolver(),
         Settings.Secure.ANDROID_ID);
+        return id;
+    }
+
+    public static String AndroidId (){
+        return id;
     }
 
 }
